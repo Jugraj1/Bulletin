b79ea255358d975d62cbaf4fa99a77ed7d9b1460
Noah Vendrig <noahvendrig@gmail.com>
2024-04-20 00:00:39 +1000

    Added Adith's changes for Tokeniser, Parser and createPost functionality

---
+++ app_2100/app/src/main/java/com/example/app_2100/FinalSearchInterpreter.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class FinalSearchInterpreter {
+    public static List<Query> process(String input){
+        List<Query> list = new ArrayList<>();
+        List<List<String>> list1 = QueryBuilder.parse(input);
+        for(List<String> item : list1){
+            list.add(QueryInterprate.Interprate(item));
+            System.out.println(QueryInterprate.Interprate(item));
+        }
+        return list;
+    }
+}
---
+++ app_2100/app/src/main/java/com/example/app_2100/User.java
@@ -0,0 +1,20 @@
+package com.example.app_2100;
+
+public class User {
+    private static User instance;
+    public String Name;
+    public String Last;
+    public String Id;
+
+    private User() {
+        // Private constructor to prevent instantiation from outside
+    }
+
+    public static User getCurrent() {
+        if (instance == null) {
+            instance = new User();
+        }
+        return instance;
+    }
+}
+
