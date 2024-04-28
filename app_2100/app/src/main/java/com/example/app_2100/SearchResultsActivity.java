a1553d7a7a9e6bcf46a8679df4751d55af6409b0
Jugraj <u7614074@anu.edu.au>
2024-04-29 04:12:48 +1000

    (Jinzheng and Jugraj) timestamp can now be compared in SearchResultsActivity

--- app_2100/app/src/main/java/com/example/app_2100/SearchResultsActivity.java
+++ app_2100/app/src/main/java/com/example/app_2100/SearchResultsActivity.java
@@ -1,20 +1,39 @@
 package com.example.app_2100;
 
+
 import android.os.Bundle;
 import android.util.Log;
 
-import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.Timestamp;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
 public class SearchResultsActivity extends AppCompatActivity {
+    private static final String TAG = "SearchResultsActivity_Screen";
+    List<Post> posts = new ArrayList<Post>();
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_search_results);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
@@ -27,9 +46,35 @@ public class SearchResultsActivity extends AppCompatActivity {
         String dateButtonString = extras.getString("dateButton");
         String dateButtonToString = extras.getString("dateButtonTo");
 
-        Log.d("searchFieldString:", searchFieldString);
-        Log.d("dateButtonString:", dateButtonString);
-        Log.d("dateButtonToString:", dateButtonToString);
+
+
+//        Log.d("crt", new Timestamp(new Date(dateButtonString)).toString());
+        Timestamp tmFrom = new Timestamp(new Date(dateButtonString));
+        Timestamp tmTo = new Timestamp(new Date(dateButtonToString));
+
+
+        db.collection("posts")
+                .whereLessThan("timeStamp", tmTo)
+                .whereGreaterThan("timeStamp", tmFrom)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+                            }
+                        } else {
+                            Log.d(TAG, "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
+
 
     }
+
+
+
+
 }
+
