2223277daf75ba49854338e2d9d63a5e7d243ed4
u7527120 <u7527120@anu.edu.au>
2024-05-06 23:26:21 +1000

    Cleaned MainActivity.java Cleared all comments

--- app_2100/app/src/main/java/com/example/app_2100/MainActivity.java
+++ app_2100/app/src/main/java/com/example/app_2100/MainActivity.java
@@ -21,37 +21,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        FirebaseUser currentUser = FirebaseAuthConnection.getCurrentUser();
-//
-//        if(currentUser != null) { // user is logged in already
-//            startActivity(new Intent(MainActivity.this, HomeFeed.class));
-////            Log.d(TAG, "logged in already");
-//        } else { // no currently logged in user
-//            startActivity(new Intent(MainActivity.this, Login.class)); // route to login screen
-//        }
+        FirebaseUser currentUser = FirebaseAuthConnection.getCurrentUser();
 
-
-        startActivity(new Intent(MainActivity.this, Login.class));
-//        startActivity(new Intent(MainActivity.this, HomeFeed.class));
-//        startActivity(new Intent(MainActivity.this, PostViewActivity.class));
-
-        // create instance of firestore DB
-//        FirebaseFirestore db = FirebaseFirestore.getInstance();
-//
-//        // READ from the database in collection named "posts" - example code, put somewhere else later
-//        db.collection("posts")
-//                .get()
-//                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-//                    @Override
-//                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-//                        if (task.isSuccessful()) {
-//                            for (QueryDocumentSnapshot document : task.getResult()) {
-//                                Log.d("Firestore READ success", document.getId() + " => " + document.getData());
-//                            }
-//                        } else {
-//                            Log.w("Firestore READ error", "Error getting documents.", task.getException());
-//                        }
-//                    }
-//                });
+        if(currentUser != null) { // user is logged in already
+            startActivity(new Intent(MainActivity.this, HomeFeed.class));
+//            Log.d(TAG, "logged in already");
+        } else { // no currently logged in user
+            startActivity(new Intent(MainActivity.this, Login.class)); // route to login screen
+        }
     }
 }
