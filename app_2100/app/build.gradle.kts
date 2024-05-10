da2f70931e6fcddfa6aaaf38787f278fc04d4ec6
Noah Vendrig <noahvendrig@gmail.com>
2024-05-10 14:38:35 +1000

    changed and fixed so everything works for target 33. possibly dangerous

--- app_2100/app/build.gradle.kts
+++ app_2100/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 android {
     namespace = "com.example.app_2100"
-    compileSdk = 34
+    compileSdk = 33
 
     defaultConfig {
         applicationId = "com.example.app_2100"
@@ -39,8 +39,9 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.activity:activity:1.8.0")
-    implementation("androidx.core:core-ktx:2.2.0") // for notifications
+    implementation("androidx.activity:activity:1.7.0")
+    implementation("androidx.fragment:fragment:1.6.0")
+    implementation("androidx.core:core-ktx:1.1.0") // for notifications
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
