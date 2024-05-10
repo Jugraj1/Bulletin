c04588c13b29a694533eb6b70798e3488fa71c17
Noah Vendrig <noahvendrig@gmail.com>
2024-05-10 14:38:02 +1000

    Asks for notif perm on api 33. test more to confirm tho

--- app_2100/app/src/main/java/com/example/app_2100/MainActivity.java
+++ app_2100/app/src/main/java/com/example/app_2100/MainActivity.java
@@ -1,18 +1,34 @@
 package com.example.app_2100;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationManagerCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 
 import com.example.app_2100.data_generators.PostGenerator;
 import com.example.app_2100.data_generators.UserGenerator;
+import com.example.app_2100.notification.Notification;
+import com.example.app_2100.notification.NotificationFactory;
+import com.example.app_2100.notification.NotificationType;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
     private static String TAG = "MainActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,13 +38,65 @@ public class MainActivity extends AppCompatActivity {
 //        FirebaseAuth.getInstance().signOut();
 
 //        generateUsers(25);
-        generatePosts(1);
+//        generatePosts(1);
 
-        if(currentUser != null) { // user is logged in already
+
+        createNotificationChannel();
+
+
+        if (currentUser != null) { // user is logged in already
             startActivity(new Intent(MainActivity.this, HomeFeed.class));
             Log.d(TAG, "logged in already");
         } else { // no currently logged in user
             startActivity(new Intent(MainActivity.this, Login.class)); // route to login screen
+        }
+    }
+
+
+
+    @RequiresApi(api = Build.VERSION_CODES.TIRAMISU)
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is not in the Support Library.
+        String CHANNEL_ID = "channel_1";
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.channel_name);
+            String description = getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this.
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+
+            Notification notif = NotificationFactory.createNotification(NotificationType.NEW_POST);
+
+            if (ContextCompat.checkSelfPermission(
+                    this, Manifest.permission.POST_NOTIFICATIONS) ==
+                    PackageManager.PERMISSION_GRANTED) {
+                Log.d(TAG, "we have perm");
+                // You can use the API that requires the permission.
+
+            } else if (ActivityCompat.shouldShowRequestPermissionRationale(
+                    this, Manifest.permission.POST_NOTIFICATIONS)) {
+                // In an educational UI, explain to the user why your app requires this
+                // permission for a specific feature to behave as expected, and what
+                // features are disabled if it's declined. In this UI, include a
+                // "cancel" or "no thanks" button that lets the user continue
+                // using your app without granting the permission.
+//                showInContextUI(...);
+                // tell user to fuck themselves
+            } else {
+                Log.d(TAG, "no perm");
+                // You can directly ask for the permission.
+                // The registered ActivityResultCallback gets the result of this request.
+                requestPermissionLauncher.launch(
+                        Manifest.permission.POST_NOTIFICATIONS);
+            }
+
+            Log.d(TAG, "sending notif");
+            notificationManager.notify(1, notif.getNotificationBuilder().build());
         }
     }
 
@@ -45,6 +113,20 @@ public class MainActivity extends AppCompatActivity {
             gen.uploadUser(email, fName, lName);
         }
     }
+
+    private ActivityResultLauncher<String> requestPermissionLauncher =
+            registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
+                if (isGranted) {
+                    // Permission is granted. Continue the action or workflow in your
+                    // app.
+                } else {
+                    // Explain to the user that the feature is unavailable because the
+                    // feature requires a permission that the user has denied. At the
+                    // same time, respect the user's decision. Don't link to system
+                    // settings in an effort to convince the user to change their
+                    // decision.
+                }
+            });
 
     private void generatePosts(int n) {
         PostGenerator gen = new PostGenerator(n, new InitialisationCallback() {
--- app_2100/app/src/main/java/com/example/app_2100/notification/Notification.java
+++ app_2100/app/src/main/java/com/example/app_2100/notification/Notification.java
@@ -1,14 +1,39 @@
 package com.example.app_2100.notification;
 
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+
+import androidx.core.app.NotificationCompat;
+
+import com.example.app_2100.App;
+import com.example.app_2100.R;
+
 public abstract class Notification {
+    // uses:
+    // https://www.gofpattern.com/creational/patterns/factoryMethodPattern-java-code.php
     private NotificationType notifType = null;
+
+    private NotificationCompat.Builder notificationBuilder;
     public Notification(NotificationType notifType){
         this.notifType = notifType;
-        setup();
+        setup(App.getContext());
     }
 
-    private void setup() {
-        // Do one time processing here
+    private void setup(Context context) {
+
+        String CHANNEL_ID = "channel_1";// The id of the channel.
+        notificationBuilder = new NotificationCompat.Builder(context, CHANNEL_ID)
+                .setSmallIcon(R.mipmap.ic_launcher_round)
+                .setContentTitle("title")
+                .setContentText("message")
+//                .setAutoCancel(true)
+                .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+    }
+
+    public NotificationCompat.Builder getNotificationBuilder() {
+        return this.notificationBuilder;
     }
 
     protected abstract void construct();
