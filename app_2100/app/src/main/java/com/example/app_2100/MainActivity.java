1c2b2bd6ae88e5ca067745dc1e372121f7106c84
Adith <u7623871@anu.edu.au>
2024-05-15 01:10:26 +1000

    Fixed Like Button in PostViewer and XML for Profileviewer

--- app_2100/app/src/main/java/com/example/app_2100/MainActivity.java
+++ app_2100/app/src/main/java/com/example/app_2100/MainActivity.java
@@ -34,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        FirebaseUser currentUser = FirebaseAuthConnection.getCurrentUser();
+        FirebaseUser currentUser = FirebaseAuthConnection.getCurrentUser();
 //        FirebaseAuth.getInstance().signOut();
 
 //        generateUsers(25);
@@ -46,12 +46,12 @@ public class MainActivity extends AppCompatActivity {
         startActivity(new Intent(MainActivity.this, Login.class));
 
 
-//        if (currentUser != null) { // user is logged in already
-//            startActivity(new Intent(MainActivity.this, HomeFeed.class));
-//            Log.d(TAG, "logged in already");
-//        } else { // no currently logged in user
-//            startActivity(new Intent(MainActivity.this, Login.class)); // route to login screen
-//        }
+        if (currentUser != null) { // user is logged in already
+            startActivity(new Intent(MainActivity.this, HomeFeed.class));
+            Log.d(TAG, "logged in already");
+        } else { // no currently logged in user
+            startActivity(new Intent(MainActivity.this, Login.class)); // route to login screen
+        }
     }
 
 
